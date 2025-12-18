bd2aeb4fc1366f5447c87a0c81c1ddbc3fa47840
İpek Gözlek <ipekgozlek@gmail.com>
2025-12-18 22:53:32 +0300

    update

---
+++ React-First/first-app/src/components/First.jsx
@@ -0,0 +1,12 @@
+
+const First = ({isim,yas}) => {
+  return (
+    <>
+    <div style={{ backgroundColor: 'blue', padding:"2rem" }}>First</div>
+    <div>İsim: {isim}</div>
+    <div>Yaş: {yas}</div>
+  </>
+  )
+}
+
+export default First
