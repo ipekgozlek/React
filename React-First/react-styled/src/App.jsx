5cf834e91a02872f6871f9c1c5b7f8c3ac1c19d1
İpek Gözlek <ipekgozlek@gmail.com>
2025-12-24 22:34:16 +0300

    useRef

--- React-First/react-styled/src/App.jsx
+++ React-First/react-styled/src/App.jsx
@@ -1,18 +1,36 @@
-import { useState } from 'react'
-import styled from 'styled-components'
+import { useState, useRef, useEffect } from 'react'
+// import styled from 'styled-components'
 import './App.css'
 
 function App() {
-   const Button1= styled.button`
-    background-color: blue;
-    color: white;
-    padding: 10px 20px;
-    font-size: 1.5rem;`;
+  const divRef = useRef(); //undefined gelme sebebi
+  console.log("başlangıç değeri",divRef.current);
+  useEffect(() => {
+    console.log(divRef.current);
+    divRef.current.style.backgroundColor = 'lightblue';
+    divRef.current.style.padding = '20px';
+    divRef.current.style.marginTop = '20px';
+  } );
+  //  const Button1= styled.button`
+  //   background-color: blue;
+  //   color: white;
+  //   padding: 10px 20px;
+  //   font-size: 1.5rem;`;
 
-  return (
+
+  //   const Button2= styled(Button1)`
+  //     background-color: red;
+  //     &:hover {
+  //       background-color: green;
+  //     }
+  //   `;
+
+    return (
     <>
-      <button>Click Me</button>
+      {/* <button>Click Me</button>
       <Button1>Styled Button</Button1>
+      <Button2>Styled Button2</Button2> */}
+      <div ref={divRef}>Ref ile hedef div</div>
     </>
   )
 }
---
+++ React-First/react-styled/src/components/Portal/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+
+const index = (target, text) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
